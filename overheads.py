3d996f8340e7d578d4f5676573e0ff373fee8b69
RobinYu3 <robinyuzh2005@gmail.com>
2023-02-01 13:29:34 +0800

    Robin Yu

---
+++ overheads.py
@@ -0,0 +1,35 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+for expense in overhead_list:
+    # Check if the current expense's percentage is greater than the current highest percentage
+    if float(expense[1]) > highest_percentage:
+    # Update the highest category and highest percentage if the current expense's category and percentage is greater than the previous high
+        highest_category = expense[0]
+        highest_percentage = float(expense[1])
+# create the summary_report text file
+fp_cwd = Path.cwd()/"summary_report.txt"
+fp_cwd.touch()
+# opens the file to write the output
+with fp_cwd.open(mode="w", encoding = "UTF-8", newline ="") as file:
+    file.write(f"[HIGHEST OVERHEADS]{highest_category.upper()}: {highest_percentage}%\n")
+
+        
+
