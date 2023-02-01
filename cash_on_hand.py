84ccf160c9157f4802ab5534d7515767a27fbb08
vernonAlgo2 <s10239948@connect.np.edu.sg>
2023-02-01 13:04:59 +0800

    Vernon's second commit

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -17,3 +17,21 @@ with fp.open(mode="r", encoding="UTF-8", newline="") as file:
 # keeps track of the previous day's cash on hand
 previous_day = cash_on_hand_list[0][0]
 previous_coh = float(cash_on_hand_list[0][1])
+
+fp_cwd = Path.cwd()/"summary_report.txt"
+# opens the file to append the output
+with fp_cwd.open(mode="a", encoding = "UTF-8", newline ="") as file:
+    # Iterate through all elements in cash_on_hand_list starting from the second element
+    # loop starts from the second element because the first element is being used as a reference point for comparison
+    for index in range(1, len(cash_on_hand_list)):
+        # Get current day and cash on hand
+        current_day = cash_on_hand_list[index][0]
+        current_coh = float(cash_on_hand_list[index][1])
+        # Check if curret cash on hand is lower than previous cash on hand
+        if current_coh < previous_coh:
+            # Calculate the difference between the current and previous cash on hand
+            difference = previous_coh - current_coh
+            file.write(f"[CASH DEFICIT] DAY: {current_day} , AMOUNT: USD{difference}\n")
+        # store current values to "previous_day" and "previous_coh" to be used as new reference point for next iteration
+        previous_day = current_day
+        previous_coh = current_coh
